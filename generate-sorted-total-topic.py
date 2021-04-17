e6b43ac16420817679aaf353efdb979f6031aba2
Douglas Figueroa <douglasr.figueroa@gmail.com>
2021-04-17 01:59:45 -0600

    Added first version of the project

---
+++ generate-sorted-total-topic.py
@@ -0,0 +1,22 @@
+"""
+This process reads an input kafka topic with ten partitions, where each
+partition has an array of ascending sorted integers, and unifies the arrays
+in a new sorted array containing all the integers and loads it in a output
+kafka topic.
+"""
+import json
+
+
+def function():
+    """
+    """
+    return 1
+
+
+def main():
+    """
+    """
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/config-producer/config_topic.py
@@ -0,0 +1,13 @@
+"""
+This process creates and configures the two kafka topics to be used.
+The input-topic with ten partitions and the output-topic with one partition.
+"""
+from kafka import KafkaAdminClient
+
+
+def main():
+    
+
+
+if __name__ == '__main__':
+    main()
