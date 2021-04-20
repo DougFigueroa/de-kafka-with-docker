480af5313ebfce64902531cabd14769cd9b1ce9c
Douglas Figueroa <douglasr.figueroa@gmail.com>
2021-04-19 20:37:55 -0600

    Added in memory producer to the simple consumer. Add array sorting and producer to write the data to the output topic.

--- src/consumer/kafka_consumer.py
+++ src/consumer/kafka_consumer.py
@@ -22,10 +22,11 @@ BROKER_CONFIG = {
     'group.id': "ConsumerGroup1",
     'auto.offset.reset': 'smallest'
 }
-POLLING_TIMEOUT = 1  # miliseconds
+POLLING_TIMEOUT = 2  # seconds
 MIN_COMMIT_COUNT = 2
 
-def consume_data(topic: list) -> None:
+
+def consume_data(topic: list) -> list:
     """Consume the data from the input-topic and write it to the output-topic.
     The data is going to be sorted in an ascending order.
     Args:
@@ -34,6 +35,7 @@ def consume_data(topic: list) -> None:
     """
     consumer = Consumer(BROKER_CONFIG)
     running = True
+    all_messages = []
     try:
         logging.info(f'Starting consuming messages from {topic}')
         consumer.subscribe(topic)
@@ -41,7 +43,9 @@ def consume_data(topic: list) -> None:
         total = 0
         while running:
             msg = consumer.poll(timeout=POLLING_TIMEOUT)
+            print(msg)
             if not msg:
+                logger.info('No message continue.')
                 continue
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
@@ -53,26 +57,36 @@ def consume_data(topic: list) -> None:
                     logger.error(f'Error while polling. {msg.error()}')
                     raise KafkaException(msg.error())
             else:
-                logging.info(f'Processing message: {msg.value()}')
-                total += int(msg.value())
-                logging.info(f'Total: {total}')
+                message_value = int(msg.value().decode('utf-8'))
+                print(type(message_value))
+                logging.info(f'Processing message: {message_value}')
+                total += message_value
+                # add to a list to order the values and loaded to the output topic
+                all_messages.append(message_value)
                 # load_data_output_topic(msg)
                 msg_count += 1
                 if msg_count % MIN_COMMIT_COUNT == 2:
                     consumer.commit(async=False)
     except KeyboardInterrupt as ki:
-        logger.warning('Process stopped by the user.')
+        logger.warning('>>> Process stopped by the user. Thanks for consuming data. :)')
         running = False
+        all_messages.sort()
+        load_data_output_topic('output_topic', all_messages)
     finally:
         # Close down consumer to commit final offsets.
         consumer.close()
 
 
-def shutdown():
-    running = False
+def acked(err, msg) -> None:
+    """
+    """
+    if err:
+        logger.error(f'Failed to deliver message: {str(msg)}, {str(err)}')
+    else:
+        logging.info(f'Message produced: {str(msg)}')
 
 
-def load_data_output_topic(topic: str, message: object) -> None:
+def load_data_output_topic(topic: str, all_messages: list) -> None:
     """Loads the sample data to the input kafka topic.
     This will load data across 10 different partitions.
     Args:
@@ -84,16 +98,15 @@ def load_data_output_topic(topic: str, message: object) -> None:
     producer = Producer(BROKER_CONFIG)
 
     # iterate through partitions
-    for data in sample_data:
-        for number in data['values']:
-            try:
-                producer.produce(topic, str(number), None, data['partition'])
-            except Exception as e:
-                logger.error(
-                    f'Producer failed to produce a message to the topic. {e}')
-                raise Exception(
-                    f'Failed to produce a message from Kakfia. {e}')
-            producer.poll(0)
+    for data in all_messages:
+        try:
+            producer.produce(topic, value=str(number), partition=0, callback=acked)
+        except Exception as e:
+            logger.error(
+                f'Producer failed to produce a message to the topic. {e}')
+            raise Exception(
+                f'Failed to produce a message from Kakfia. {e}')
+        producer.poll(0)
 
     # ensure all the delivery queue has been loaded
     producer.flush()
